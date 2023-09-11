9c7545b2cbd624f1d1c2fe8e8ab265b45c187d92
se0hyun <kimlisa00@seoultech.ac.kr>
2023-09-11 09:39:23 +0900

    feat(se0hyun_230828): 료aximum Subarray 완

---
+++ leetCode/Maximum_Subarray_se0hyun.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = [num for num in nums]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i - 1] + nums[i], nums[i])
+
+        return max(dp)
