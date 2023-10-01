9f4f2556605b7f12c0e108f470ac268063c85b81
eun61n00 <eun61n@gmail.com>
2023-10-01 11:00:58 +0900

    feat(eun61n00_230925): Q15649 풀이 추가

--- baekjoon/Q15649_eun61n00.py
+++ baekjoon/Q15649_eun61n00.py
@@ -6,7 +6,26 @@ from itertools import permutations
 
 n, m = map(int, input().split())
 
+# 풀이 1
 num_list = [_ for _ in range(1, n + 1)]
 
 for p in permutations(num_list, m):
     print(*p)
+
+
+# 풀이 2
+answer = []
+
+
+def backtracking():
+    if len(answer) == m:
+        print(" ".join(map(str, answer)))
+        return
+    for i in range(1, n + 1):
+        if i not in answer:
+            answer.append(i)
+            backtracking()
+            answer.pop()
+
+
+backtracking()
