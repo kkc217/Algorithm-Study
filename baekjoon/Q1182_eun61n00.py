0a04886bc62d741d5fc09f8e5263b4e8ce345525
eun61n00 <eun61n@gmail.com>
2023-10-07 14:48:55 +0900

    feat(eun61n00_230925): Q1182 백트랙킹 풀이 추가

--- baekjoon/Q1182_eun61n00.py
+++ baekjoon/Q1182_eun61n00.py
@@ -6,33 +6,30 @@ from itertools import combinations
 
 
 n, s = map(int, input().split())
-num_list = list(map(int, input().split()))
+sequence = list(map(int, input().split()))
 count = 0
 
-# 풀이 1 백트랙킹을 이용한 풀이 (정답 X)
+# 풀이 1 백트랙킹을 이용한 풀이
 
 
-def back_tracking(lst, idx):
-    global count
-    if lst and sum(lst) == s:
-        print(lst)
+def backtracking(part_sequence, idx):
+    global answer, answer_list
+    if sum(part_sequence) == s and len(part_sequence) > 0:
         count += 1
-        return
-    for i in range(num_list[idx], n):
-        if num_list[i] not in lst:
-            lst.append(num_list[i])
-            print(idx, lst)
-            back_tracking(lst, idx + 1)
-            lst.pop()
+    for i in range(idx + 1, len(sequence)):
+        num = sequence[i]
+        part_sequence.append(num)
+        backtracking(part_sequence, i)
+        part_sequence.pop()
 
 
-back_tracking([], 0)
+backtracking([], -1)
 print(count)
 
 
 # 풀이 2 combinations를 이용한 풀이
 for i in range(1, n + 1):
-    for c in combinations(num_list, i):
+    for c in combinations(sequence, i):
         if sum(c) == s:
             count += 1
 
